b2a90d9fba949d60100be3ecd02efd4d65359ce2
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-12-02 21:51:57 +0530

    qualifier annotation

--- SpringCore/springcoretut/src/main/java/com/nabajyoti/springcoretut/autowire/annotation/Student.java
+++ SpringCore/springcoretut/src/main/java/com/nabajyoti/springcoretut/autowire/annotation/Student.java
@@ -1,10 +1,12 @@
 package com.nabajyoti.springcoretut.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Student {
 
     @Autowired
+    @Qualifier("address2")
     private Address address;
     
     public Address getAddress() {
